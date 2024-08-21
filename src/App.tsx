c070460dc4715a5d9851bc7dfa25575ab1d2f1c7
Luana <luana.gabriela96@hotmail.com>
2024-08-21 12:10:33 -0300

    Componente Home - CSS

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import Home from "./pages/home/Home"
+
+
+function App() {
+  // Código TypeScript
+  
+
+  return (
+    // Código TSX => HTML
+    <>
+ 
+    <Home />
+      </>
+  )
+}
+
+export default App
